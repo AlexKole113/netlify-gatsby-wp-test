987483abeaf47d41de9338d5f7621827858a73d7
AlexKole113 <68900752+AlexKole113@users.noreply.github.com>
2021-01-04 20:56:42 +0100

    Create index.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,37 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React, {Component} from "react"
+import ThemeToggler from "../ThemeToggler";
+import Menu from "../Menu";
+import './index.css'
+import Logo from "../Logo";
+
+
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        const { siteTitle } = this.props;
+        return (<header className={`header`} >
+            <div className={`headerInner row`} >
+                <div className={`col`}>
+                    <Logo />
+                </div>
+                <div className={`col-2 menu-section`}>
+                    <Menu />
+                </div>
+                <div className={`col`}>
+                    <ThemeToggler />
+                </div>
+
+            </div>
+        </header>)
+
+    }
+
+
+}
+
