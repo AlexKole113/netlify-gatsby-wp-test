d7918b0097a338a349e073b3ca1175904f55b288
AlexKole113 <alexander.koledov@gmail.com>
2021-01-05 12:57:47 +0100

    add redux in project

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,28 @@ module.exports = {
         ],
       },
     },
-      'gatsby-plugin-netlify'
+      'gatsby-plugin-netlify',
+    {
+      resolve: `gatsby-plugin-react-redux`,
+      options: {
+        // [required] - path to your createStore module
+        pathToCreateStoreModule: './src/pages/reducers/main.js',
+        // [optional] - options passed to `serialize-javascript`
+        // info: https://github.com/yahoo/serialize-javascript#options
+        // will be merged with these defaults:
+        serialize: {
+          space: 0,
+          // if `isJSON` is set to `false`, `eval` is used to deserialize redux state,
+          // otherwise `JSON.parse` is used
+          isJSON: false,
+          unsafe: true,
+          ignoreFunction: true,
+        },
+        // [optional] - if true will clean up after itself on the client, default:
+        cleanupOnClient: true,
+        // [optional] - name of key on `window` where serialized state will be stored, default:
+        windowKey: '__PRELOADED_STATE__',
+      },
+    },
   ],
 }
